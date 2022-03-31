35537f54ee816635f15b2ce58a12228aeb16b7fd
Huseyin Tas <htas767@gmail.com>
2022-03-31 13:44:33 +0300

    object detection added

--- app/src/main/java/com/example/myeyes/activites/DetectorActivity.java
+++ app/src/main/java/com/example/myeyes/activites/DetectorActivity.java
@@ -34,11 +34,11 @@ public class DetectorActivity extends CameraActivity implements OnImageAvailable
 
     private static final int TF_OD_API_INPUT_SIZE = 300;
     private static final boolean TF_OD_API_IS_QUANTIZED = true;
-    private static final String TF_OD_API_MODEL_FILE = "detect.tflite";
+    private static final String TF_OD_API_MODEL_FILE = "detect_quant_meta.tflite";
     private static final String TF_OD_API_LABELS_FILE = "file:///android_asset/labelmap.txt";
     private static final DetectorMode MODE = DetectorMode.TF_OD_API;
     // Minimum detection confidence to track a detection.
-    private static final float MINIMUM_CONFIDENCE_TF_OD_API = 0.55f;
+    private static final float MINIMUM_CONFIDENCE_TF_OD_API = 0.6f;
 
     private static final Size DESIRED_PREVIEW_SIZE = new Size(640, 480);
 
